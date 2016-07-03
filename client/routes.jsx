46393ac1db40d78c9b47b15f2b9327470623545a
Maricha <maricha17@gazeta.pl>
2016-07-03 13:12:50 +0200

    fix

--- client/routes.jsx
+++ client/routes.jsx
@@ -4,6 +4,7 @@ import {mount} from 'react-mounter';
 import {MainLayout} from './layouts/MainLayout.jsx';
 import App from '../App.jsx';
 
+
 FlowRouter.route('/', {
   action() {
     mount(MainLayout, {
